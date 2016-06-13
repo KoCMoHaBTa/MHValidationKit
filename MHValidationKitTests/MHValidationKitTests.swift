e6c6320691113bf6f04362ba8e08868583d809cd
Milen Yordanov Halachev <milenski88@gmail.com>
2016-06-13 15:40:37 +0300

    - added operator tests to validator

--- MHValidationKitTests/MHValidationKitTests.swift
+++ MHValidationKitTests/MHValidationKitTests.swift
@@ -12,25 +12,44 @@ import XCTest
 class MHValidationKitTests: XCTestCase {
     
     override func setUp() {
+        
         super.setUp()
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
+        
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        
         super.tearDown()
     }
     
-    func testExample() {
+    func testValidatorOperators() {
+        
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTAssertEqual((AnyValidator<String>(value: true) || AnyValidator<String>(value: true)).validate(nil), true || true)
+        XCTAssertEqual((AnyValidator<String>(value: true) || AnyValidator<String>(value: false)).validate(nil), true || false)
+        XCTAssertEqual((AnyValidator<String>(value: false) || AnyValidator<String>(value: true)).validate(nil), false || true)
+        XCTAssertEqual((AnyValidator<String>(value: false) || AnyValidator<String>(value: false)).validate(nil), false || false)
+        
+        XCTAssertEqual((AnyValidator<String>(value: true) && AnyValidator<String>(value: true)).validate(nil), true && true)
+        XCTAssertEqual((AnyValidator<String>(value: true) && AnyValidator<String>(value: false)).validate(nil), true && false)
+        XCTAssertEqual((AnyValidator<String>(value: false) && AnyValidator<String>(value: true)).validate(nil), false && true)
+        XCTAssertEqual((AnyValidator<String>(value: false) && AnyValidator<String>(value: false)).validate(nil), false && false)
     }
+}
+
+extension AnyValidator {
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    public init(@autoclosure(escaping) value: () -> Bool) {
+        
+        self.init { _ -> Bool in
+            
+            return value()
         }
     }
-    
 }
