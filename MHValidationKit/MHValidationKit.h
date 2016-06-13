fedf8be3b0f92337cd435b5b1d3dee6be51fbef9
Milen Yordanov Halachev <milenski88@gmail.com>
2016-06-13 15:12:31 +0300

    Initial Commit

---
+++ MHValidationKit/MHValidationKit.h
@@ -0,0 +1,19 @@
+//
+//  MHValidationKit.h
+//  MHValidationKit
+//
+//  Created by Milen Halachev on 6/13/16.
+//  Copyright © 2016 Milen Halachev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MHValidationKit.
+FOUNDATION_EXPORT double MHValidationKitVersionNumber;
+
+//! Project version string for MHValidationKit.
+FOUNDATION_EXPORT const unsigned char MHValidationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MHValidationKit/PublicHeader.h>
+
+
